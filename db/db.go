6b4227193832c6c860e5a3ea79f4f96598f41d3e
wujuno <huj88854@gmail.com>
2023-08-01 08:32:59 +0900

    DB연결 구현

---
+++ db/db.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func Init() (*sql.DB, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbName
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("DB에 연결되었습니다.🔗")
+	return db, nil
+
+}
--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"fullstackBoard/db"
 	"fullstackBoard/pkg/httpserver"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	//DB연결
+	fmt.Println("DB에 연결합니다.")
+	db, err := db.Init()
+	if err != nil {
+		fmt.Println("DB에 연결에 실패했습니다.:", err)
+		os.Exit(1)
+	}
+	defer db.Close()
 
 	//서버 시작
 	fmt.Println("8080 포트에서 서버를 시작합니다🚀")
